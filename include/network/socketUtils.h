c5ec027bbdc08f16dc4b11affadf56b160ab9a24
Anubhav <anubhavsah0@gmail.com>
2026-07-21 13:07:50 +0530

    Add socket receive timeout for chunk downloads (#8)

--- include/network/socketUtils.h
+++ include/network/socketUtils.h
@@ -5,3 +5,7 @@ int acceptClient(int serverSocketFd);
 int connectToServer(const char* ip, const int port);
 void closeSocket(int socketFd);
 ssize_t sendAll(int socketFd, const char* buffer, int byteRead);
+
+bool setSocketRecvTimeout(int socketFd, int timeoutSeconds);
+bool setSocketSendTimeout(int socketFd, int timeoutSeconds);
+bool isSocketTimeoutError();
